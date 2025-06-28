eac32151c8d09b5a00f3eab319e978488350324f
Sienna Frederic <sienna.frederic@duke.edu>
2025-06-28 11:36:47 -0400

    added axios

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-console.log(app)
+app.use(cors())
 
-app.listen(8080, () => {
-	console.log('server listening on port 8080')
+app.get('/', (req, res) => {
+      res.send('Hello from our server!')
 })
 
+app.listen(8080, () => {
+      console.log('server listening on port 8080')
+})
